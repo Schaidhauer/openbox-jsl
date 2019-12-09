19dc63685e120732f623fe7fef510d75becc25dd
Lennon da Silva Rocha <lennon.silva95@gmail.com>
2019-12-09 18:27:43 -0300

    Passa chave por parâmetro na chamada da JSL

--- vars/awsAnsible.groovy
+++ vars/awsAnsible.groovy
@@ -1,7 +1,7 @@
 def call(Map params) {
     sh 'echo "' + libraryResource('Dockerfile') + '" | ' +
        'docker build --rm ' +
-       '--build-arg SSH_PRIVATE_KEY="' + credentials('jenkins-git-ansible-key') + '" ' +
+       '--build-arg SSH_PRIVATE_KEY="' + params.sshGitKey + '" ' +
        '--no-cache -t ansible-docker:latest -'
 
     sh 'docker run --rm ansible-docker:latest ansible-playbook ' + 
