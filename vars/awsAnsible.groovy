c0bad2598f4e8ee009e25ddf3cb85e7f2371b109
Lennon da Silva Rocha <lennon.silva95@gmail.com>
2019-12-09 14:15:10 -0300

    Correção erro de sintaxe

--- vars/awsAnsible.groovy
+++ vars/awsAnsible.groovy
@@ -1,6 +1,6 @@
 def call(Map params) {
     sh 'docker build ../resources/ -t ansible-docker:latest'
-    sh 'docker run --rm ansible-docker:latest ansible-playbook' + '
+    sh 'docker run --rm ansible-docker:latest ansible-playbook ' + 
        '/ansible/' + params.playbook + '--extra-vars "{'
        'deploy: ' + params.deploy + ',' +
        'ec2_access_key: ' + params.accessKey + ',' +
