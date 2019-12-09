0d5083ab0b49a11b6418c75a0b29cc7f4ea96cb9
Lennon da Silva Rocha <lennon.silva95@gmail.com>
2019-12-09 19:01:12 -0300

    Aspa simples no echo para não substituir variáveis do docker

--- vars/awsAnsible.groovy
+++ vars/awsAnsible.groovy
@@ -1,12 +1,12 @@
 def call(Map params) {
-    sh 'echo "' + libraryResource('Dockerfile') + '" | ' +
-       'docker build --rm ' +
-       '--build-arg SSH_PRIVATE_KEY="' + params.sshGitKey + '" ' +
-       '--no-cache -t ansible-docker:latest -'
+    sh "echo '" + libraryResource('Dockerfile') + "' | " +
+       "docker build --rm " +
+       "--build-arg SSH_PRIVATE_KEY='" + params.sshGitKey + "' " +
+       "--no-cache -t ansible-docker:latest -"
 
-    sh 'docker run --rm ansible-docker:latest ansible-playbook ' + 
-       '/ansible/' + params.playbook + '--extra-vars "{'
-       'deploy: ' + params.deploy + ',' +
-       'ec2_access_key: ' + params.accessKey + ',' +
-       'ec2_secret_key: ' + params.secretKey + '}"'
+    sh "docker run --rm ansible-docker:latest ansible-playbook " + 
+       "/ansible/" + params.playbook + "--extra-vars '{"
+       "deploy: "  + params.deploy + "," +
+       "ec2_access_key: " + params.accessKey + "," +
+       "ec2_secret_key: " + params.secretKey + "}'"
 }
