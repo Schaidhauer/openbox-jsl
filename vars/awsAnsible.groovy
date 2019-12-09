75660643179142ddd0de514c59be2ad8ee653408
Lennon da Silva Rocha <lennon.silva95@gmail.com>
2019-12-09 19:13:33 -0300

    Acho que agora sim

--- vars/awsAnsible.groovy
+++ vars/awsAnsible.groovy
@@ -1,15 +1,12 @@
 def call(Map params) {
-    dFile = libraryResource('Dockerfile')
-    sh "echo '" + dFile  + "'"
+    sh 'echo "' + libraryResource('Dockerfile') + '" | ' +
+       'docker build --rm ' +
+       '--build-arg SSH_PRIVATE_KEY="' + credentials('jenkins-git-ansible-key') + '" ' +
+       '--no-cache -t ansible-docker:latest -'
 
-    sh "echo '" + dFile + "' | " +
-       "docker build --rm " +
-       "--build-arg SSH_PRIVATE_KEY='" + params.sshGitKey + "' " +
-       "--no-cache -t ansible-docker:latest -"
-
-    sh "docker run --rm ansible-docker:latest ansible-playbook " + 
-       "/ansible/" + params.playbook + "--extra-vars '{"
-       "deploy: "  + params.deploy + "," +
-       "ec2_access_key: " + params.accessKey + "," +
-       "ec2_secret_key: " + params.secretKey + "}'"
+    sh 'docker run --rm ansible-docker:latest ansible-playbook ' + 
+       '/ansible/' + params.playbook + '--extra-vars "{'
+       'deploy: '  + params.deploy + ',' +
+       'ec2_access_key: ' + params.accessKey + ',' +
+       'ec2_secret_key: ' + params.secretKey + '}"'
 }
