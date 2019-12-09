555862c43cd3ff6614f619be571c071c27b254f6
Lennon da Silva Rocha <lennon.silva95@gmail.com>
2019-12-09 14:57:05 -0300

    Altera para não haver necessidade de helper functions

--- vars/awsAnsible.groovy
+++ vars/awsAnsible.groovy
@@ -1,8 +1,34 @@
 def call(Map params) {
-    sh 'docker build ../resources/ -t ansible-docker:latest'
+    sh 'echo "' + script.libraryResource('Dockerfile')  +'" | docker build --no-cache -t ansible-docker:latest -'
     sh 'docker run --rm ansible-docker:latest ansible-playbook ' + 
        '/ansible/' + params.playbook + '--extra-vars "{'
        'deploy: ' + params.deploy + ',' +
        'ec2_access_key: ' + params.accessKey + ',' +
        'ec2_secret_key: ' + params.secretKey + '}"'
+}
+
+/**
+ * Generates a path to a temporary file location, ending with {@code path} parameter.
+ *
+ * @param path path suffix
+ * @return path to file inside a temp directory
+ */
+@NonCPS
+String createTempLocation(String path) {
+    String tmpDir = script.pwd tmp: true
+    return tmpDir + File.separator + new File(path).getName()
+}
+
+/**
+ * Returns the path to a temp location of a script from the global library (resources/ subdirectory)
+ *
+ * @param srcPath path within the resources/ subdirectory of this repo
+ * @param destPath destination path (optional)
+ * @return path to local file
+ */
+String copyGlobalLibraryScript(String srcPath, String destPath = null) {
+    destPath = destPath ?: createTempLocation(srcPath)
+    script.writeFile file: destPath, text: script.libraryResource(srcPath)
+    script.echo "copyGlobalLibraryScript: copied ${srcPath} to ${destPath}"
+    return destPath
 }
