6c5fb81a8e9e8982c3ff28c00dc0bb441c973a54
Lennon da Silva Rocha <lennon.silva95@gmail.com>
2019-12-18 16:54:23 -0300

    Faltou fechar as aspas duplas

--- vars/awsAnsible.groovy
+++ vars/awsAnsible.groovy
@@ -5,7 +5,7 @@ def call(Map params) {
 
     sh "mkdir " + sshKeyDir + " || rm -f " + sshKeyDir + "/*"
     sh "cp " + params.sshGitKey + " " + sshKeyDir + "/" + sshKeyFile
-    sh 'echo "' + libraryResource('Dockerfile') // + '" > ' sshKeyDir + '/Dockerfile'
+    sh 'echo "' + libraryResource('Dockerfile') + '"' // + '" > ' sshKeyDir + '/Dockerfile'
 
 /*    sh 'docker build --rm --build-arg SSH_PRIVATE_KEY_FILE=' + sshKeyFile +
        '--no-cache -f ' + sshKeyDir + '/Dockerfile -t ansible-docker:latest ' + sshKeyDir
