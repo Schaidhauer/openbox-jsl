feff83a91e0cd00c56e164348a69d9c14ada2aa8
Lennon da Silva Rocha <lennon.silva95@gmail.com>
2019-12-18 18:03:37 -0300

    Correção de sintaxe, faltou um sinal de +

--- vars/awsAnsible.groovy
+++ vars/awsAnsible.groovy
@@ -14,7 +14,7 @@ def call(Map params) {
     sh 'rm -rf ' + sshKeyDir
 
     sh 'docker run --rm ansible-docker:latest ansible-playbook ' + 
-       '/ansible/' + params.playbook + '--extra-vars "{'
+       '/ansible/' + params.playbook + '--extra-vars "{' +
        'deploy: '  + params.deploy + ',' +
        'ec2_access_key: ' + params.accessKey + ',' +
        'ec2_secret_key: ' + params.secretKey + ',' +
