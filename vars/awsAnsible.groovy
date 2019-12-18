3a3d41141919cc9603fd82b652f5202c64b553c8
Lennon da Silva Rocha <lennon.silva95@gmail.com>
2019-12-18 17:12:46 -0300

    Volta com os comandos de montar a imagem e executar o container

--- vars/awsAnsible.groovy
+++ vars/awsAnsible.groovy
@@ -6,9 +6,9 @@ def call(Map params) {
     sh "mkdir " + sshKeyDir + " || rm -f " + sshKeyDir + "/*"
     sh "cp " + params.sshGitKey + " " + sshKeyDir + "/" + sshKeyFile
     sh "echo -n '" + libraryResource('Dockerfile') + "' > /var/jenkins_home/tmp/Dockerfile"
-    sh 'cat ' + sshKeyDir + '/Dockerfile'
+//    sh 'cat ' + sshKeyDir + '/Dockerfile'
 
-/*    sh 'docker build --rm --build-arg SSH_PRIVATE_KEY_FILE=' + sshKeyFile +
+    sh 'docker build --rm --build-arg SSH_PRIVATE_KEY_FILE=' + sshKeyFile +
        '--no-cache -f ' + sshKeyDir + '/Dockerfile -t ansible-docker:latest ' + sshKeyDir
 
     sh 'rm -rf ' + sshKeyDir
@@ -18,5 +18,5 @@ def call(Map params) {
        'deploy: '  + params.deploy + ',' +
        'ec2_access_key: ' + params.accessKey + ',' +
        'ec2_secret_key: ' + params.secretKey + ',' +
-       'key_name: ' + params.keyname  + '}"'*/
+       'key_name: ' + params.keyname  + '}"'
 }
