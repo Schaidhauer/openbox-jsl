4a9094b396037369f6978090e9d272c162e69d5d
Lennon da Silva Rocha <lennon.silva95@gmail.com>
2020-01-08 16:12:47 -0300

    Passa a chave do repositório a partir do Jenkins

--- vars/awsAnsible.groovy
+++ vars/awsAnsible.groovy
@@ -3,21 +3,30 @@ def call(Map params) {
     sshKeyFile = "rsa.key"
     sshKeyDir = "/var/jenkins_home/tmp"
 
+    // Cria diretório para armazenar as chaves
     sh "mkdir " + sshKeyDir + " || rm -f " + sshKeyDir + "/*"
+
+    // Cria arquivos com chaves de acesso aos repositórios do aws-ansible e do projeto em questão
     sh "cp " + params.sshGitKey + " " + sshKeyDir + "/" + sshKeyFile
+    sh "cp " + params.sshRepoKey + " " + sshKeyDir + "/repokey"
     sh "echo -n '" + libraryResource('Dockerfile') + "' > /var/jenkins_home/tmp/Dockerfile"
 
+    // Cria imagem do aws-ansible
     sh 'docker build --rm --build-arg SSH_PRIVATE_KEY_FILE=' + sshKeyFile + ' ' +
        '--no-cache -f ' + sshKeyDir + '/Dockerfile -t ansible-docker:latest ' + sshKeyDir
 
-    sh 'rm -rf ' + sshKeyDir
-
-    sh 'docker run --rm ansible-docker:latest ansible-playbook ' + 
+    // Executa o ansible para deploy na AWS
+    sh 'docker run --rm --mount type=bind,source=' + sshKeyDir  + ',destination=/ansible/ssh ' +
+       'ansible-docker:latest ansible-playbook ' + 
        '/ansible/' + params.playbook + ' --extra-vars "{' +
        'deploy: '  + params.deploy + ',' +
        'ec2_access_key: ' + params.accessKey + ',' +
        'ec2_secret_key: ' + params.secretKey + ',' +
        'key_name: ' + params.keyname  + '}"'
 
+    // Remove imagem após uso
     sh 'docker rmi -f ansible-docker:latest'
+
+    // Chaves já foi utilizadas, então deleta
+    sh 'rm -rf ' + sshKeyDir
 }
